815cb03765074417f7db859ef8e44b630fe34a66
shadow931 <59968327+shadow931@users.noreply.github.com>
2021-04-17 12:31:00 +0530

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -42,4 +42,47 @@ class Stackk:
             temp=temp.next
         print()
 
+
+       
     
+    
+useme = Stackk()
+
+
+while True:
+
+    print("\n1.Push\n2.Pop\n3.Length\n4.Top Element\n5.Display Stack\n6.Exit\n")
+    value = int(input("Enter the choice"))
+
+    if value in [1,2,3,4,5,6]:
+
+        if value == 1:
+            useme.push(int(input("Enter the value to be pushed ")))
+            continue
+
+        elif value == 2:
+            useme.pop()
+            continue
+
+        elif value == 3:
+            useme.len()
+            continue
+
+        elif value == 4:
+            useme.top()
+            continue
+
+        elif value == 5:
+            useme.display()
+            continue
+
+        elif value == 6:
+            exit()
+
+        break
+    else:
+        print("invalid input")
+
+
+        
+        
